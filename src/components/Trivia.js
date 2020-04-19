97edcb37da10acd1c411848fb674de0a6e2fb7fe
RadhikaChamdal <radhika_chamdal@hotmail.co.uk>
2020-04-19 17:45:06 +0100

    added in api, created new css component for api to style

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,11 +1,46 @@
 import React from "react";
 import axios from "axios";
-
-
+import styles from "./Trivia.module.css";
 
 function Trivia() {
- 
+    const [questionString, setQuestionString] = React.useState([]);
+    const [showAnswer, setShowAnswer] = React.useState(false);
+  
+    React.useEffect(() => {
+      const promise = axios.get(
+      "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean"
+      );
+      promise.then(response => {
+        console.log(response.data.results);
+        setQuestionString(response.data.results);
+      });
+    }, []);
+    const toggleShowAnswer = () => {
+      setShowAnswer(!showAnswer);
+    };
+  
+    const question = questionString.map((result, index) => {
+      return (
+        <div key={index}>
+          <p>{result.question}</p>
+          <button onClick={toggleShowAnswer}> Reveal</button>
+          {showAnswer}
+  
+          {/* <p>{result.correct_answer}</p> */}
+        </div>
+      );
+    });
+  
+    if (questionString) {
+      return (
+        <div className = {styles.question}>
+          <p>{question}</p>
+        </div>
+      );
+    } else {
+      return <p> Waiting for a queston </p>;
+    }
+  }
     
-}
    
 export default Trivia
